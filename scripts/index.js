ebc052257c377fc03d6fdddae6f0730e3a963573
ezztherose <ezztherose@gmail.com>
2019-10-17 13:37:02 +0200

    fixed files

--- scripts/index.js
+++ scripts/index.js
@@ -1,68 +1,48 @@
 /* Open the sidenav */
-function openNav() {
+function openNav() 
+{
     document.getElementById("mySidenav").style.width = "100%";
 }
 
 /* Close/hide the sidenav */
-function closeNav() {
+function closeNav() 
+{
     document.getElementById("mySidenav").style.width = "0";
 }
 
-
-
-
-
+/* ===== SLIDER ===== */
 var slideIndex = 1;
 
 showSlides(slideIndex);
 
-
-
-function plusSlides(n) {
-
+function plusSlides(n) 
+{
     showSlides(slideIndex += n);
-
 }
 
-
-
-function currentSlide(n) {
-
+function currentSlide(n) 
+{
     showSlides(slideIndex = n);
-
 }
 
-
-
-function showSlides(n) {
-
+function showSlides(n) 
+{
     var i;
-
     var slides = document.getElementsByClassName("mySlides");
 
-    var dots = document.getElementsByClassName("dot");
-
-    if (n > slides.length) { slideIndex = 1 }
-
-    if (n < 1) { slideIndex = slides.length }
-
-    for (i = 0; i < slides.length; i++) {
-
-        slides[i].style.display = "none";
-
+    if (n > slides.length) 
+    {
+        slideIndex = 1
     }
 
-    for (i = 0; i < dots.length; i++) {
+    if (n < 1) {
+        slideIndex = slides.length
+    }
 
-        dots[i].className = dots[i].className.replace(" active", "");
-
+    for (i = 0; i < slides.length; i++) 
+    {
+        slides[i].style.display = "none";
     }
 
     slides[slideIndex - 1].style.display = "block";
-
-    dots[slideIndex - 1].className += " active";
-
 }
-
-
-
